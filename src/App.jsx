cca27ede48c84b4895651bcf177b53217bb24e46
Anita George <anitageorge1806@gmail.com>
2025-08-04 17:31:10 +0530

    Added comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-
+// Cooollll CSS imports
 // Import all your components
 import Header from './components/Header';
 import Home from './components/Home';
